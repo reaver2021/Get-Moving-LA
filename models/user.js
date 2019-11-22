62c5568577f1fa655f25af4d08f83da4fbbec852
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-22 09:02:42 -0800

    Changed UserModel to Users

--- models/user.js
+++ models/user.js
@@ -12,6 +12,6 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const UserModel = mongoose.model('User', UserSchema);
+const Users = mongoose.model('User', UserSchema);
 
-module.exports = UserModel
+module.exports = Users;
