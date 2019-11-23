4e376c15d3eead2e6f5fb7af79d421837c33c530
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-22 16:06:46 -0800

    Added place api

--- client/util/placeAPI.js
+++ client/util/placeAPI.js
@@ -5,19 +5,22 @@ const place_key = process.env.PLACES_API_KEY;
 export default {
 
     getNewJobs: function() {
-        return axios.get(`http://api.adzuna.com:80/v1/api/jobs/gb/search/1?app_id=${place_id}&app_key=
-        ${place_key}&results_per_page=20&what=""&where=losangeles1&content-type=application/json`);
+        return axios.get(`https://api.foursquare.com/v2/venues/search?
+        near=Los Angeles, CA&
+        client_id=${place_id}&
+        client_secret=${place_key}&
+        query=""`);
     },
 
     getSavedJobs: function() {
-        return axios.get("/api/jobs/");
+        return axios.get("/api/places/");
     },
 
     deleteJobs: function(id){
-        return axions.delete("/api/jobs" + id);
+        return axions.delete("/api/places" + id);
     },
 
     saveJobs: function(jobData){
-        return axios.post("/api/jobs", jobData);
+        return axios.post("/api/places", placeData);
     }
 };
