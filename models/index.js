182951762d23eb723eda365d0a32f225ec05cff7
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-22 17:19:27 -0800

    Changed db's around

---
+++ models/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Jobs: require('./jobs'),
+    Places: require('./places'),
+    Profile: require('./profile'),
+    Users: require('./user')
+};
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const dbconnection = require('./db/connection')
 const PORT = process.env.PORT || 3000;
 
 
