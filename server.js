60eb930184d28f0de47a77a3a322df134a53ea12
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-11-30 14:15:37 -0800

    took out cors as it messed with heroku

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 
- 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-   
-app.listen(3000, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
